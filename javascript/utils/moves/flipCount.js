0c55ca124acc85680a0a4710fd3b794a6350d27d
Adam  Eichman <adameichman@MacBook-Pro-658.lan>
2025-02-18 12:43:44 -0500

    Changing isValidMove to flipCount to track a moves impact

--- javascript/utils/moves/flipCount.js
+++ javascript/utils/moves/flipCount.js
@@ -1,39 +1,42 @@
-function isValidMove(player, board, row, col) {
+function flipCount(player, board, row, col) {
     /**
-     * Checks if the move is valid and would benifit the players score
+     * Checks all possible moves at a certain position and returns the count of best 
      * @param {number} player - The player number (1 or 2)
      * @param {Array} board - The game board (8x8 array)
      * @param {number} row - The x coordinate of the move 
      * @param {number} col - The y coordinate of the move
+     * @return {number} - The number of pieces that would be flipped if the move is played
      */
 
     const directions = [ [1, 0], [0, 1], [1, 1], [-1, 0], [0, -1], [-1, -1], [-1, 1], [1, -1] ];
 
     if (board[row][col] !== 0) {
-        return false;
+        return 0;
     }
 
     const opponent = 3 - player; 
+    let totalFlipped = 0;
 
     for (let [dx, dy] of directions) {
         let i = row + dx;
         let j = col + dy;
-        let hasOpponentBetween = false;         // Only true if move would increase player's score
-
-        
+        let flipped = 0;
+         
         while (i >= 0 && i < 8 && j >= 0 && j < 8) { 
             if (board[i][j] === opponent) {
-                hasOpponentBetween = true;
-            } else if (board[i][j] === player && hasOpponentBetween) {
-                return true;
+                flipped++;
+            } else if (board[i][j] === player) {
+                totalFlipped += flipped;
+                break;
             } else {
                 break;
             }
             i += dx;
             j += dy;
+
         }
     }
 
-    return false;
+    return totalFlipped;
 }
-module.exports = { isValidMove };
+module.exports = { flipCount };
